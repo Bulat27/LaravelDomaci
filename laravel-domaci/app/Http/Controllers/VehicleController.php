7efcd277fdd622e021dfd9f2bbfd1ff56b12b7f0
Bulat27 <nbulat99@gmail.com>
2022-05-01 16:48:00 +0200

    Implementirana obrada create i delete zahteva za Vehicle

--- laravel-domaci/app/Http/Controllers/VehicleController.php
+++ laravel-domaci/app/Http/Controllers/VehicleController.php
@@ -6,6 +6,7 @@ use App\Models\Vehicle;
 use Illuminate\Http\Request;
 use App\Http\Resources\VehicleResource;
 use App\Http\Resources\VehicleCollection;
+use Illuminate\Support\Facades\Validator;
 
 class VehicleController extends Controller
 {
@@ -39,7 +40,34 @@ class VehicleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator=Validator::make($request->all(),[
+            'color'=>'required|String|max:255',
+            'fuel_type'=>'required|String|max:255',
+            'manufacture_year'=>'required|Integer|max:2022',
+            'engine_capacity'=>'required|between:1,5',
+            'mileage'=>'required|Integer|max:500000',
+            'manufacturer_id'=>'required',
+            'vehicle_type_id'=>'required'
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors());
+        }
+
+        $vehicle = new Vehicle;
+        $vehicle->color = $request->color;
+        $vehicle->fuel_type = $request->fuel_type;
+        $vehicle->manufacture_year = $request->manufacture_year;
+        $vehicle->engine_capacity = $request->engine_capacity;
+        $vehicle->mileage = $request->mileage;
+        $vehicle->manufacturer_id= $request->manufacturer_id;
+        $vehicle->vehicle_type_id= $request->vehicle_type_id;
+
+        if($vehicle->save()){
+            return response()->json(['Vehicle has been saved successfully!', new VehicleResource($vehicle)]);
+        }
+
+        return response()->json('Vehicle has not been saved successfully!', 500);
     }
 
     /**
@@ -73,7 +101,33 @@ class VehicleController extends Controller
      */
     public function update(Request $request, Vehicle $vehicle)
     {
-        //
+        // $validator=Validator::make($request->all(),[
+        //     'color'=>'required|String|max:255',
+        //     'fuel_type'=>'required|String|max:255',
+        //     'manufacture_year'=>'required|Integer|max:2022',
+        //     'engine_capacity'=>'required|between:1,5',
+        //     'mileage'=>'required|Integer|max:500000',
+        //     'manufacturer_id'=>'required',
+        //     'vehicle_type_id'=>'required'
+        // ]);
+
+        // if($validator->fails()){
+        //     return response()->json($validator->errors());
+        // }
+
+        // $vehicle->color = $request->color;
+        // $vehicle->fuel_type = $request->fuel_type;
+        // $vehicle->manufacture_year = $request->manufacture_year;
+        // $vehicle->engine_capacity = $request->engine_capacity;
+        // $vehicle->mileage = $request->mileage;
+        // $vehicle->manufacturer_id= $request->manufacturer_id;
+        // $vehicle->vehicle_type_id= $request->vehicle_type_id;
+
+        // if($vehicle->update()){
+        //     return response()->json(['Vehicle has been updated successfully!', new VehicleResource($vehicle)]);
+        // }
+
+        // return response()->json('Vehicle has not been updated successfully!', 500);
     }
 
     /**
@@ -84,6 +138,10 @@ class VehicleController extends Controller
      */
     public function destroy(Vehicle $vehicle)
     {
-        //
+        if($vehicle->delete()){
+            return response()->json('Vehicle has been successfully deleted!');
+        }
+
+        return response()->json('Vehicle deletion has failed!', 500);
     }
 }
